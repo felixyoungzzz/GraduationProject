d50de7d85be488c681f9c9caa1477502ddfbfc22
felix <felixyoungzzz@163.com>
2018-03-14 10:50:55 +0800

    add realtimestock api (not index)

--- server/controllers/realtimestock.js
+++ server/controllers/realtimestock.js
@@ -4,15 +4,34 @@ async function getrealtimeStock(ctx) {
   let indexCode = ctx.params.indexCode;
   let res = await axios.get('http://hq.sinajs.cn/list=' + indexCode);
   let result = res.data.split(',');
-  if (!result[3].includes('-')) {
-    result[2] = '+' + result[2];
-    result[3] = '+' + result[3];
+  console.log(result);
+  if (indexCode.includes('_')) {
+    if (!result[3].includes('-')) {
+      result[2] = '+' + result[2];
+      result[3] = '+' + result[3];
+    }
+    ctx.body = {
+      current: result[1],
+      change: result[2],
+      percentage: result[3],
+    };
+  } else {
+    let change = result[3] - result[2];
+    let percentage = change / result[3] * 100;
+    if (change > 0) {
+      ctx.body = {
+        current: result[3],
+        change: '+' + change.toFixed(3).toString(),
+        percentage: '+' + percentage.toFixed(2).toString(),
+      };
+    } else {
+      ctx.body = {
+        current: result[3],
+        change: change.toFixed(3).toString(),
+        percentage: percentage.toFixed(2).toString(),
+      };
+    }
   }
-  ctx.body = {
-    current: result[1],
-    change: result[2],
-    percentage: result[3],
-  };
 }
 
 module.exports = {
